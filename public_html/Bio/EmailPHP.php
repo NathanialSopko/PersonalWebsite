cbacd4a21e3850cf16e0e190aa2751c1ab7f80c0
nksopko@buffalo.edu <Motox#798>
2018-05-01 14:26:52 -0400

    init

---
+++ public_html/Bio/EmailPHP.php
@@ -0,0 +1,16 @@
+<?php
+// the message
+$theirName= $_GET['Name'];
+$to = "natesopko@gmail.com";
+$subject = $_GET['Subject'];
+$message = $_GET['Message'];
+$email = $_GET['Email'];
+$headers = '';
+// use wordwrap() if lines are longer than 70 characters
+$message = wordwrap($message,70);
+
+$headers = "From:". $email . "\r\n" .
+// send email
+mail($to,$subject,$message, $headers);
+echo "<script>window.close();</script>";
+?>
